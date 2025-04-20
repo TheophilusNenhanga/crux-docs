75548d1bdb5c615852f206d2650295fa643618dc
TheophilusNenhanga <theonenhanga@gmail.com>
2025-04-20 13:42:52 -0600

    feat: added pages
    - added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  site: 'https://theophilusnenhanga.github.io',
-  base: '/',
-  output: 'static'
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	site: "https://theophilusnenhanga.github.io",
+	base: "/",
+	output: "static",
+	adapter: netlify(),
 });
